2983488e0425f024ffcedcc331c01bd0ebe6229f
yinglichen <443926536@qq>
2019-11-07 20:24:58 +0800

    homePage

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+   
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+    }
+  },
+  methods:{
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
